fcf30b4355b9d08f51030fda22f5c1721d00431e
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2021-12-28 01:47:49 -0500

    Update Tareas

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 /// Modifica el componente para que se puedan agregar tareas
 
 const App = () => {
-  const addTask = input => {
-    if (input.key === "Enter" && input.target.value !== "") {
-      const todo = document.querySelector(".todo");
-      const li = document.createElement("li");
-      li.innerHTML = input.target.value;
-      todo.append(li);
-
-      const inputText = document.querySelector("#new-task");
-      inputText.value = "";
+  const [inputValue, setInputValue] = useState("");
+  const [taks, setTask] = useState([
+    "Sacar la ropa",
+    "Hacer la cama",
+    "Leer un rato",
+  ]);
+  const loadValue = value => {
+    setInputValue(value);
+  };
+  const handlerSubmit = event => {
+    event.preventDefault();
+    if (inputValue.length > 0) {
+      setTask([...taks, inputValue]);
+      setInputValue("");
     }
   };
+
   return (
     <div className="wrapper">
       <div className="list">
         <h3>Por hacer:</h3>
         <ul className="todo">
-          <li>Sacar la ropa</li>
-          <li>Hacer la cama</li>
-          <li>Leer un rato</li>
+          {taks.map(t => (
+            <li key={t}>{t}</li>
+          ))}
         </ul>
-        <form>
+        <form onSubmit={e => handlerSubmit(e)}>
           <input
             type="text"
             id="new-task"
-            onKeyUp={e => addTask(e)}
-            onChange={e => addTask(e)}
+            onChange={e => loadValue(e.target.value)}
+            value={inputValue}
             placeholder="Ingresa una tarea y oprime Enter"
           />
         </form>
