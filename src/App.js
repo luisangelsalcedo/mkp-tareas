e38e7fa8446c2729da3c47a69d4dc5532835138c
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2021-12-27 22:54:34 -0500

    add Tareas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React, { Component } from 'react';
+import React from "react";
 
 /// Modifica el componente para que se puedan agregar tareas
 
-class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
-          </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
-        </div>
-      </div>
-    )
-  }
-}
+const App = () => {
+  const addTask = input => {
+    if (input.key === "Enter" && input.target.value !== "") {
+      const todo = document.querySelector(".todo");
+      const li = document.createElement("li");
+      li.innerHTML = input.target.value;
+      todo.append(li);
 
+      const inputText = document.querySelector("#new-task");
+      inputText.value = "";
+    }
+  };
+  return (
+    <div className="wrapper">
+      <div className="list">
+        <h3>Por hacer:</h3>
+        <ul className="todo">
+          <li>Sacar la ropa</li>
+          <li>Hacer la cama</li>
+          <li>Leer un rato</li>
+        </ul>
+        <form>
+          <input
+            type="text"
+            id="new-task"
+            onKeyUp={e => addTask(e)}
+            onChange={e => addTask(e)}
+            placeholder="Ingresa una tarea y oprime Enter"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,71 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { render } from 'enzyme';
-import { shallow } from 'enzyme';
-import { mount } from 'enzyme';
-import App from './App';
-import Setup from './Setup';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "enzyme";
+import { shallow } from "enzyme";
+import { mount } from "enzyme";
+import App from "./App";
+import Setup from "./Setup";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
 });
 
-it('renders one ul tag', () => {
+it("renders one ul tag", () => {
   const wrapper = render(<App />);
-  expect(wrapper.find('ul').length).toBe(1);
+  expect(wrapper.find("ul").length).toBe(1);
 });
 
-it('renders three li tags at the beggining', () => {
+it("renders three li tags at the beggining", () => {
   const wrapper = render(<App />);
-  expect(wrapper.find('li').length).toBe(3);
+  expect(wrapper.find("li").length).toBe(3);
 });
 
-it('rendered the li with correct text at the beggining', () => {
+it("rendered the li with correct text at the beggining", () => {
   const wrapper = shallow(<App />);
-  const tasks = ['Sacar la ropa', 'Hacer la cama', 'Leer un rato']
-  expect(wrapper.find('li').length).toBe(3);
-  wrapper.find('li').forEach(function(node, i) {
+  const tasks = ["Sacar la ropa", "Hacer la cama", "Leer un rato"];
+  expect(wrapper.find("li").length).toBe(3);
+  wrapper.find("li").forEach(function (node, i) {
     expect(node.text()).toBe(tasks[i]);
   });
 });
 
-it('creates a new task with the correct text', () => {
+it("creates a new task with the correct text", () => {
   const wrapper = mount(<App />);
-  const tasks = ['Sacar la ropa', 'Hacer la cama', 'Leer un rato']
-  const newTask = wrapper.find('#new-task');
-  newTask.simulate('change', { target: { value: 'Hola' }})
-  newTask.simulate('keyDown', { key: 'Enter' })
-  newTask.simulate('keyPress', { key: 'Enter' })
-  newTask.simulate('keyUp', { key: 'Enter' })
-  if(wrapper.find('li').length  === 4)
-    expect(wrapper.find('li').length).toBe(4);
+  const tasks = ["Sacar la ropa", "Hacer la cama", "Leer un rato"];
+  const newTask = wrapper.find("#new-task");
+  newTask.simulate("change", { target: { value: "Hola" } });
+  newTask.simulate("keyDown", { key: "Enter" });
+  newTask.simulate("keyPress", { key: "Enter" });
+  newTask.simulate("keyUp", { key: "Enter" });
+  if (wrapper.find("li").length === 4)
+    expect(wrapper.find("li").length).toBe(4);
   else {
-    newTask.simulate('change', { target: { value: 'Hola' }})
-    wrapper.find('form').simulate('submit', newTask)
-    expect(wrapper.find('li').length).toBe(4);
+    newTask.simulate("change", { target: { value: "Hola" } });
+    wrapper.find("form").simulate("submit", newTask);
+    expect(wrapper.find("li").length).toBe(4);
   }
-  wrapper.find('li').forEach(function(node, i) {
-    if(i == 3)
-      expect(node.text()).toBe('Hola');
-    else
-      expect(node.text()).toBe(tasks[i]);
+  wrapper.find("li").forEach(function (node, i) {
+    if (i == 3) expect(node.text()).toBe("Hola");
+    else expect(node.text()).toBe(tasks[i]);
   });
-})
+});
 
-it('the text input value is reset after creating task', () => {
+it("the text input value is reset after creating task", () => {
   const wrapper = mount(<App />);
-  const newTask = wrapper.find('#new-task');
-  newTask.getElement.value = 'Hola';
-  newTask.simulate('change', { target: { value: 'Hola' }})
-  newTask.simulate('keyDown', { key: 'Enter' })
-  newTask.simulate('keyPress', { key: 'Enter' })
-  newTask.simulate('keyUp', { key: 'Enter' })
-  if(wrapper.find('#new-task').getElement().props.value == '')
-    expect(wrapper.find('#new-task').getElement().props.value).toBe('')
+  const newTask = wrapper.find("#new-task");
+  newTask.getElement.value = "Hola";
+  newTask.simulate("change", { target: { value: "Hola" } });
+  newTask.simulate("keyDown", { key: "Enter" });
+  newTask.simulate("keyPress", { key: "Enter" });
+  newTask.simulate("keyUp", { key: "Enter" });
+  if (wrapper.find("#new-task").getElement().props.value == "")
+    expect(wrapper.find("#new-task").getElement().props.value).toBe("");
   else {
-    newTask.simulate('change', { target: { value: 'Hola' }})
-    wrapper.find('form').simulate('submit', newTask)
-    expect(wrapper.find('#new-task').getElement().props.value).toBe('')
+    newTask.simulate("change", { target: { value: "Hola" } });
+    wrapper.find("form").simulate("submit", newTask);
+    expect(wrapper.find("#new-task").getElement().props.value).toBe("");
   }
-})
+});
